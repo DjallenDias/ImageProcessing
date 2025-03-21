f5a7757e137495ec59641777292573f3ae38f099
Djállen Fabrício <djallendias@gmail.com>
2025-03-21 12:09:11 -0300

    IM - rgb to img function implemented

--- ImageManipulation/src/image_processing.py
+++ ImageManipulation/src/image_processing.py
@@ -1,5 +1,6 @@
 import numpy as np
 import PIL.Image
+from PIL import Image
 
 import file_reading
 
@@ -14,5 +15,16 @@ def _img_to_rgb_arr(img_name: str):
     r = np.array(img.getchannel("R"))
     g = np.array(img.getchannel("G"))
     b = np.array(img.getchannel("B"))
-    
+
     return r, g, b
+
+def _rgb_to_img(r: np.ndarray, g: np.ndarray, b: np.ndarray):
+    r_img = Image.fromarray(r)
+    g_img = Image.fromarray(g)
+    b_img = Image.fromarray(b)
+
+    image_r = Image.merge("RGB", (r_img, Image.new("L", r_img.size, 0), Image.new("L", r_img.size, 0)))
+    image_g = Image.merge("RGB", (Image.new("L", g_img.size, 0), g_img, Image.new("L", g_img.size, 0)))
+    image_b = Image.merge("RGB", (Image.new("L", b_img.size, 0), Image.new("L", b_img.size, 0), b_img))
+
+    return Image.merge("RGB", (r_img, g_img, b_img))
